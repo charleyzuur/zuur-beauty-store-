2980c548e68b26df32b8c43c152aeb81fb58c88d
charleyzuur <132037573+charleyzuur@users.noreply.github.com>
2023-04-29 04:35:26 +1200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,68 @@
+import product1 from "./assets/1.jpeg";
+import product2 from "./assets/2.jpeg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpeg";
+import product5 from "./assets/5.jpeg";
+import product6 from "./assets/6.jpeg";
+import product7 from "./assets/7.jpeg";
+import product8 from "./assets/8.jpeg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "The Starter Ritual",
+        price: 79.99,
+        productImage: product1,
+
+    },
+    {
+        id: 2,
+        productName: "The Protect and Prime Ritual",
+        price: 89.99,
+        productImage: product2,
+
+    },
+    {
+        id: 3,
+        productName: "The Clarifying Care Set",
+        price: 59.99,
+        productImage: product3,
+
+    },
+    {
+        id: 4,
+        productName: "The Soothing Care Set",
+        price: 79.99,
+        productImage: product4,
+
+    },
+    {
+        id: 5,
+        productName: "The Replenishing Care Setl",
+        price: 79.99,
+        productImage: product5,
+
+    },
+    {
+        id: 6,
+        productName: "The Soothing Ritual",
+        price: 69.99,
+        productImage: product6,
+
+    },
+    {
+        id: 7,
+        productName: "The Cleansing Ritual",
+        price: 99.99,
+        productImage: product7,
+
+    },
+    {
+        id: 8,
+        productName: "The Cleanse and Nourish Set",
+        price: 79.99,
+        productImage: product8,
+
+    },
+];
+/* these lines of code represent the images on my website*/
